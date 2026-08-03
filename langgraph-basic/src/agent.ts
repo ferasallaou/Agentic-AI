4179fb90777fc835f376a365a351e03fbd07e506
Feras Allao <feras.allaou@gmail.com>
2026-08-03 17:04:26 +0300

    First usage of langGraph

---
+++ langgraph-basic/src/agent.ts
@@ -0,0 +1,60 @@
+import { END, START, StateGraph, StateSchema } from "@langchain/langgraph";
+import { ChatOpenRouter } from "@langchain/openrouter";
+
+import { createAgent, SystemMessage } from "langchain";
+import z from "zod";
+
+const graphState = new StateSchema({
+  title: z.string(),
+  outline: z.string(),
+  post: z.string(),
+});
+
+const LLM = new ChatOpenRouter({
+  model: process.env.AI_MODEL,
+});
+
+async function outLineAgent(graphState) {
+  const agent = createAgent({
+    model: LLM,
+  });
+
+  const result = await agent.invoke({
+    messages: [
+      new SystemMessage(
+        `Create a blog post outline for this topic: ${graphState.title}`,
+      ),
+    ],
+  });
+
+  return { outline: result.messages.at(-1)?.content };
+}
+
+async function blogPostAgent(graphState) {
+  const agent = createAgent({
+    model: LLM,
+  });
+
+  const result = await agent.invoke({
+    messages: [
+      new SystemMessage(
+        `Create a blog post for this topic: ${graphState.title} \n
+        Following this outline ${graphState.outline}`,
+      ),
+    ],
+  });
+
+  return { post: result.messages.at(-1)?.content };
+}
+
+const graph = new StateGraph(graphState)
+  .addNode("outLineAgent", outLineAgent)
+  .addNode("blogPostAgent", blogPostAgent)
+  .addEdge(START, "outLineAgent")
+  .addEdge("outLineAgent", "blogPostAgent")
+  .addEdge("blogPostAgent", END)
+  .compile();
+
+export async function invokeGraph(topic: string) {
+  return await graph.invoke({ title: topic });
+}
